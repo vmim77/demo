6468f13da657b10f9fae25244ebcc4a9db92d107
Parksunghyun <shpark3149@raoncorp.com>
2023-08-22 15:41:45 +0900

    modify HelloController.java

--- src/main/java/com/example/demo/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/demo/controller/HelloController.java
@@ -1,6 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HelloController {
+
+    @GetMapping(value = "/")
+    public String index() {
+        return "hello index";
+    }
     
 }
